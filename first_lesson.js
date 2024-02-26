b3d09ef730c58c3fd8ab40a7743d40d0423d52f0
Nguyen Thuy <anhquan0905@gmail.com>
2024-02-26 23:38:02 +0700

    learning variable, let, var, const

---
+++ first_lesson.js
@@ -0,0 +1,91 @@
+// console.log("Hello world !");
+
+// console.log(1+2);
+
+//variable
+// x='Thuy Nguyen';
+// console.log(x);
+// x=1;
+// console.log(x);
+
+// x= true;
+// console.log(x);
+
+// let
+// {
+//     //Scope A
+//     let a= 1;
+//     a=5;
+//     {
+//         //Scope B
+//         console.log(a);
+
+//         {
+//             //Scope C
+//             a=6; 
+//             console.log(a);
+
+//         }
+//     }
+// }
+
+// {
+//     //Scope A
+    
+//     {
+//         //Scope B
+//         let a= 1;
+//         console.log(a);
+
+//         {
+//             //Scope C
+//             a=6; 
+//             console.log(a);
+
+//         }
+//     }
+//     console.log(a);
+// }
+
+// {
+    //Scope A
+    
+//     let a=1;
+    
+//     {
+//         //Scope B
+//         let a= 2;
+//         console.log("Scope B");
+//         console.log(a);
+
+//         {
+//             //Scope C
+//             console.log("Scope C");
+//             console.log(a);
+//         }
+//     }
+//     console.log("Scope A");
+//     console.log(a);
+// }
+
+//const
+// const nameThuy= "Nguyen Thi Thuy";
+// nameThuy= "Nguyen Thi";
+// {
+//     const a = 2;
+//     {
+//         const a = 3;
+//         a= 4;
+//     }
+// }
+
+// var
+
+{
+    a = 4; 
+ 
+    {
+         a = 3; 
+    }
+console.log(a);
+}
